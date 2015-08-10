b4617cfd282a49f87eaac1ebe227148bab6ff737
amessing1 <amessing@kth.se>
2015-08-10 21:54:38 +0200

    text input

--- client.c
+++ client.c
@@ -7,8 +7,11 @@
 #include <pthread.h>
 #include <assert.h>
 #include <SDL2/SDL.h>
+#include <SDL/SDL_ttf.h>
 
 
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
 char * parseName(char buffer[], int length){
     char *name = malloc(length);
@@ -34,6 +37,68 @@ void *recieveMessages(void *args){
     }
 }
 
+
+void activateInputField(SDL_Renderer *renderer, char *buffer, int *counter){
+    write(1, "Start typing\n", sizeof("Start typing\n"));
+    SDL_Surface *textSurface;
+    SDL_Texture *textTexture;
+
+
+    SDL_Rect srcRect;
+    SDL_Rect destRect;
+    srcRect.x = 0;
+    srcRect.y = 0;
+    srcRect.w = 30;
+    srcRect.h = 30;
+
+    destRect.x = SCREEN_WIDTH / 2 - 100;
+    destRect.y = SCREEN_HEIGHT - 40;
+    destRect.w = 30;
+    destRect.h = 30;
+    SDL_SetTextInputRect(&srcRect);
+    TTF_Font *font;
+    if((font = TTF_OpenFont("fonts/FreeSerif.ttf", 8)) == NULL){
+        printf("TTF_OpenFont: %s\n", TTF_GetError());
+    }
+    SDL_Color color = {0,0,0};
+
+    SDL_StartTextInput();
+    int typing = 1;
+    char *composition;
+    int cursor;
+    int selection_len;
+    while(typing){
+        SDL_Event event;
+        SDL_PollEvent(&event);
+        switch(event.type){
+            case SDL_TEXTEDITING:
+                printf("1");
+                composition = event.edit.text;
+                cursor = event.edit.start;
+                selection_len = event.edit.length;
+                printf("%s\n", composition);
+            break;
+            case SDL_TEXTINPUT:
+                strcat(buffer, event.text.text);
+                assert(font != NULL);
+                textSurface = TTF_RenderUTF8_Solid(font, buffer, color);
+                textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+        
+                SDL_RenderCopy(renderer, textTexture, &srcRect, &destRect);
+                *counter += 1;
+                SDL_RenderPresent(renderer);
+            break;
+            case SDL_QUIT:
+
+                typing = 0;
+            break;
+        }
+        
+    }
+    SDL_StopTextInput();
+    return;
+}
+
 int main(int argc, char* argv[]){
 
     ///////////
@@ -41,13 +106,13 @@ int main(int argc, char* argv[]){
     ///////////
 
     
-    const int SCREEN_WIDTH = 640;
-    const int SCREEN_HEIGHT = 480;
+    
 
     // Create window
     SDL_Window *window;
     //SDL_Surface *screen;
     SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
     window = SDL_CreateWindow("Chat program name", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     //screen = SDL_GetWindowSurface(window);
     //SDL_Surface* screenSurface;
@@ -57,13 +122,15 @@ int main(int argc, char* argv[]){
 
     SDL_Rect srcRect;
     SDL_Rect destRect;
+    SDL_Rect textInputRect;
 
     // Text input box
-
-    srcRect.x = 0;
-    srcRect.y = 0;
-    srcRect.w = SCREEN_WIDTH / 2;
-    srcRect.h = 30;
+    char textBuffer;
+    int textCounter = 0;
+    textInputRect.x = 0;
+    textInputRect.y = 0;
+    textInputRect.w = SCREEN_WIDTH / 2;
+    textInputRect.h = 30;
 
     destRect.x = SCREEN_WIDTH / 2 - 100;
     destRect.y = SCREEN_HEIGHT - 40;
@@ -75,10 +142,10 @@ int main(int argc, char* argv[]){
     if((textInputBackground = SDL_CreateRGBSurface(0, 10, 10, 32, 0, 0, 0, 0)) == NULL){
         printf("ERROR: %s\n", SDL_GetError());
     }
-    SDL_FillRect(textInputBackground, &srcRect, SDL_MapRGB(textInputBackground->format, 255, 255, 0));
+    SDL_FillRect(textInputBackground, &textInputRect, SDL_MapRGB(textInputBackground->format, 255, 255, 0));
     textInputTexture = SDL_CreateTextureFromSurface(renderer, textInputBackground);
     SDL_FreeSurface(textInputBackground);
-    SDL_RenderCopy(renderer, textInputTexture, &srcRect, &destRect);
+    SDL_RenderCopy(renderer, textInputTexture, &textInputRect, &destRect);
 
 
     // Recieve Message box
@@ -150,11 +217,39 @@ int main(int argc, char* argv[]){
     SDL_RenderCopy(renderer, friendsBoxTexture, &srcRect, &destRect);
 
     // Update the window
-    while(1){
+    int running = 1;
+    while(running){
         SDL_RenderPresent( renderer );
-        sleep(1);
+        SDL_Event event;
+        SDL_PollEvent(&event);
+        switch(event.type){
+            
+            case SDL_MOUSEBUTTONDOWN:
+                if(event.button.button == SDL_BUTTON_LEFT){
+                    printf("type: %d, BUTTON_LEFT: %d\n", event.button.button, SDL_BUTTON_LEFT);
+                    if((event.button.x > SCREEN_WIDTH / 2 - 100) && 
+                        (event.button.x < SCREEN_WIDTH - 10) &&
+                        (event.button.y > SCREEN_HEIGHT - 40) && 
+                        (event.button.y < SCREEN_HEIGHT - 10)){
+                        // Text Input
+                        activateInputField(renderer, &textBuffer, &textCounter);
+                    }
+                }
+            break;
+            case SDL_QUIT:
+                running = 0;
+            break;
+        }
+        //sleep(0.1);
+    
     }
-
+    SDL_DestroyTexture(friendsBoxTexture);
+    SDL_DestroyTexture(profileBoxTexture);
+    SDL_DestroyTexture(recieveBoxTexture);
+    SDL_DestroyTexture(textInputTexture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     /////////
 #if 0
